44c9cbac0ca067599ff70a3d55c5113f2d05b07a
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2023-10-02 07:56:38 +0400

    Using `current::{Write,Process}Blob()`.

---
+++ src/schema.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "typesystem/struct.h"
+
+CURRENT_STRUCT(Number) {
+  CURRENT_FIELD(value, uint32_t);
+  CURRENT_CONSTRUCTOR(Number)(uint32_t value) : value(value) {
+  }
+  CURRENT_CONSTRUCTOR(Number)(int value) : value(static_cast<uint32_t>(value)) {
+  }
+};
---
+++ src/write_primes.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+#include "bricks/dflags/dflags.h"
+#include "utils/blobs/blobs.h"
+
+#include "gen_primes.h"
+#include "schema.h"
+
+DEFINE_uint32(n, 100, "Up to what number should the primes be generated.");
+DEFINE_string(output, "primes.bin", "The file name to output the primes into.");
+
+int main(int argc, char **argv) {
+  ParseDFlags(&argc, &argv);
+  std::vector<Number> numbers;
+  GenPrimes(static_cast<int>(FLAGS_n), [&numbers](int p) { numbers.emplace_back(p); });
+  current::WriteBlob(numbers, FLAGS_output);
+}
